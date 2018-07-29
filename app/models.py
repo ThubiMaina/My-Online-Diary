ad1cb8f611e81f5f22f7a3d65409ea1f7f4f256f
ThubiMaina <erickmaina35@gmail.com>
2018-07-29 18:40:11 +0300

    [FIX] models to include contents

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@
 from flask_bcrypt import Bcrypt
 from flask import Flask
 import jwt
-from jwt import InvalidTokenError
+from jwt import InvalidTokenError,ExpiredSignatureError
 from datetime import datetime, timedelta
 
 class User:
@@ -42,17 +42,11 @@ class User:
 
 class DiaryEntries:
     """docstring for DiaryEntries"""
-    def __init__(self, owner, title, entry_id, date):
-        self.owner = owner
+    def __init__(self, user_id, title, entry_id, date, content):
+        self.user_id = user_id
         self.title = title
         self.entry_id = entry_id
-        self.date = datetime.utcnow()
-
-class Content:
-    def __init__(self, content_id, diary_id, content, date ):
-        self.content_id = content_id
-        self.diary_id = diary_id
         self.content = content
-        self.date = datetime.utcnow()
+        self.date = date
 
 
